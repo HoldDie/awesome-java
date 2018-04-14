75a77ba3454b6c004b63b94fe470ebd9b710cedb
holddie <holddie@163.com>
2018-04-14 08:09:25 +0800

    :ambulance: 初始化父类的普通成员变量和代码块

---
+++ niu-coder/src/com/holddie/niucoder/gan0414/test0414.java
@@ -0,0 +1,17 @@
+package holddie.niucoder.gan0414;
+
+/**
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/4/14 7:51
+ */
+public class test0414 {
+    public static void main(String[] args) {
+        byte a1 = 2, a2 = 4, a3;
+        short s = 16;
+//        byte 与 short 的转化
+//        a2 = s;
+//        a3 = a1 * a2;
+    }
+}
