f27babbcb5a8b13143cfdd8b4cf8cdb91cc793f0
holddie <holddie@163.com>
2018-04-18 09:33:27 +0800

    :disappointed_relieved: 空指针，数组越界认识

---
+++ niu--coder/src/com/holddie/niu/code/gan0418/TrueTest.java
@@ -0,0 +1,23 @@
+package com.holddie.niu.code.gan0418;
+
+/**
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/4/18 9:20
+ */
+public class TrueTest {
+    public static void main(String[] args) {
+        Boolean flag = false;
+        if (flag = true) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        System.out.println(flag = false);
+
+        int a[]=null; a[0]=1;//将产生ArraylndexOutOfBoundsException
+        a[1] =2;
+    }
+}
