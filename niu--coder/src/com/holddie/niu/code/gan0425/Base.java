16dc84165218a173d2c585bc205e4c078acf29e4
holddie <holddie@163.com>
2018-04-25 08:58:10 +0800

    :aerial_tramway: 垃圾收集，构造方法内部调用

---
+++ niu--coder/src/com/holddie/niu/code/gan0425/Base.java
@@ -0,0 +1,14 @@
+package com.holddie.niu.code.gan0425;
+
+public class Base{
+int w, x, y ,z;
+public Base(int a,int b)
+{
+x=a; y=b;
+}
+public Base(int a, int b, int c, int d)
+{
+// assignment
+x=a; y=b;
+w=d;z=c;
+}}
