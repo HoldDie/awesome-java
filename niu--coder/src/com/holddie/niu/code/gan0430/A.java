ae6715a4555ed6574ba0d04976ba57d6424b2b47
holddie <holddie@163.com>
2018-04-30 22:31:59 +0800

    :recycle: 抽象类和接口重新认识

---
+++ niu--coder/src/com/holddie/niu/code/gan0430/A.java
@@ -0,0 +1,28 @@
+package com.holddie.niu.code.gan0430;
+
+/**
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/4/30 21:22
+ */
+public class A {
+    A(){}
+}
+
+class B extends A{
+    int ii;
+
+//    B(){
+//        super();
+//    }
+
+    B(int ii) {
+        this.ii = ii;
+    }
+
+    public static void main(String[] args) {
+        B b = new B(12);
+        System.out.println(b.ii);
+    }
+}
---
+++ niu--coder/src/com/holddie/niu/code/gan0430/TestChildClass.java
@@ -0,0 +1,29 @@
+package com.holddie.niu.code.gan0430;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Method;
+
+/**
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/4/30 21:31
+ */
+public class TestChildClass {
+    public static void main(String[] args) throws Exception {
+        //获取子类
+        Class clazz = Class.forName("com.holddie.niu.code.gan0430.Child");
+        //得到父类
+        Class superClass = clazz.getSuperclass();
+        //得到父类非继承的所以方法
+        Method[] methods = superClass.getDeclaredMethods();
+        //设置私有方法可以被访问
+        AccessibleObject.setAccessible(methods, true);
+        for (Method m : methods) {
+            System.out.println();
+            System.out.println("子类调用方法" + m.getName() + "()的调用结果:");
+            m.invoke(new Child());
+        }
+
+    }
+}
