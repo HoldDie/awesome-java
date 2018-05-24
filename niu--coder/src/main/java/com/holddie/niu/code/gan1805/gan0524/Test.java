72d16b91c82c399e720996efa99ddafdaf856e3c
holddie <holddie@163.com>
2018-05-24 10:42:31 +0800

    :cd: 正则表达式实现

---
+++ niu--coder/src/main/java/com/holddie/niu/code/gan1805/gan0524/Test.java
@@ -0,0 +1,14 @@
+package com.holddie.niu.code.gan1805.gan0524;
+
+/**
+ * 测试
+ * @author yangze1
+ * @version 1.0.0
+ * @email holddie@163.com
+ * @date 2018/5/24 7:39
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a[] = {1,2,2,2,2,2};
+    }
+}
--- sword-pointing-to-offer/src/main/java/com/holddie/sword/pointing/offer/No19.java
+++ sword-pointing-to-offer/src/main/java/com/holddie/sword/pointing/offer/No19.java
@@ -1,70 +1,53 @@
 package com.holddie.sword.pointing.offer;
 
 /**
- * 删除链表中重复的结点
+ * 正则表达式匹配
  * @author yangze1
  * @version 1.0.0
  * @email holddie@163.com
- * @date 2018/5/23 8:32
+ * @date 2018/5/24 8:12
  */
 public class No19 {
 
     /**
-     * 题目描述：
-     * 假设一个链表中的值有序，进行相同的结点去重
+     * 应该注意到，'.' 是用来当做一个任意字符，而 '*' 是用来重复前面的字符。
+     * 这两个的作用不同，不能把 '.' 的作用和 '*' 进行类比，从而把它当成重复前面字符一次。
      *
-     * 思路：
-     * 那就是前后两个结点值进行比较，过滤值相同的结点
+     * if p.charAt(j) == s.charAt(i)  :  then dp[i][j] = dp[i-1][j-1];
+     * if p.charAt(j) == '.'          :  then dp[i][j] = dp[i-1][j-1];
+     * if p.charAt(j) == '*'          :
+     * if p.charAt(j-1) != s.charAt(i)  :  then dp[i][j] = dp[i][j-2]  // a* only counts as empty
+     * if p.charAt(j-1) == s.charAt(i)
+     * or p.charAt(i-1) == '.'             :
+     * then dp[i][j] = dp[i-1][j]   // a* counts as multiple a
+     * or dp[i][j] = dp[i][j-1]   // a* counts as single a
+     * or dp[i][j] = dp[i][j-2]   // a* counts as empty
      */
+    private boolean match(char[] str, char[] pattern) {
+        int m = str.length, n = pattern.length;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++)
+            if (pattern[i - 1] == '*')
+                dp[0][i] = dp[0][i - 2];
 
-    private ListNode deleteDuplication(ListNode pHead){
-        if (pHead == null || pHead.next ==null){
-            return pHead;
-        }
-        ListNode next = pHead.next;
-        if(pHead.data.equals(next.data)){
-            while (next != null && pHead.data.equals(next.data)){
-                next = next.next;
-            }
-            return deleteDuplication(next);
-        }else {
-            pHead.next = deleteDuplication(pHead.next);
-            return pHead;
-        }
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if (pattern[j - 1] == '*')
+                    if (pattern[j - 2] == str[i - 1] || pattern[j - 2] == '.')
+                        dp[i][j] = dp[i][j - 1] || dp[i][j - 2] || dp[i - 1][j];
+                    else
+                        dp[i][j] = dp[i][j - 2];
+        return dp[m][n];
     }
 
-    public ListNode deleteDuplication1(ListNode pHead) {
-        if (pHead == null || pHead.next == null)
-            return pHead;
-        ListNode next = pHead.next;
-        if (pHead.data.equals(next.data)) {
-            while (next != null && pHead.data.equals(next.data))
-                next = next.next;
-            return deleteDuplication1(next);
-        } else {
-            pHead.next = deleteDuplication1(pHead.next);
-            return pHead;
-        }
-    }
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode("A");
-        ListNode node2 = new ListNode("B");
-        ListNode node3 = new ListNode("C");
-        ListNode node4 = new ListNode("C");
-        ListNode node5 = new ListNode("C");
-
-        node1.setNext(node2);
-        node2.setNext(node3);
-        node3.setNext(node4);
-        node4.setNext(node5);
-
-
         No19 no19 = new No19();
-        no19.deleteDuplication1(node1);
-        System.out.println(node1);
+//        System.out.println(no19.match("aaa".toCharArray(),"a.a".toCharArray()));
+        System.out.println(no19.match("aaa".toCharArray(), "aa*ac*a".toCharArray()));
     }
-
-
 
 }
