534618ace8bde460f71c5e4e8e1da19fd8702c33
holddie <holddie@163.com>
2018-09-21 09:48:26 +0800

    :diamond_shape_with_a_dot_inside: 提交

--- java-base/src/com/holddie/java/base/invoke/SubjectClient.java
+++ java-base/src/com/holddie/java/base/invoke/SubjectClient.java
@@ -34,30 +34,29 @@ public class SubjectClient {
         System.out.println(subject1 instanceof Proxy);
 
         System.out.println(subject1.getClass().toString());
-        System.out.println("subject 中的属性有：");
+        System.out.print("subject 中的属性有：");
         Field[] fields = subject1.getClass().getDeclaredFields();
         for (Field fiel :
                 fields) {
             System.out.print(fiel.getName() + ",");
         }
 
-        System.out.println("subject 中的方法有：");
+        System.out.print("\nsubject 中的方法有：");
         Method[] methods = subject1.getClass().getDeclaredMethods();
         for (Method meth :
                 methods) {
             System.out.print(meth.getName() + ",");
         }
 
-        System.out.println();
-        System.out.println("subject 的父类是：" + subject1.getClass().getSuperclass());
-        System.out.println("subject 的实现接口是：");
+        System.out.println("\nsubject 的父类是：" + subject1.getClass().getSuperclass());
+        System.out.print("subject 的实现接口是：");
         Class[] interfaces = subject1.getClass().getInterfaces();
         for (Class clss :
                 interfaces) {
             System.out.print(clss.getName() + ",");
         }
 
-        System.out.println("运行结果为：");
+        System.out.println("\n运行结果为：");
         subject1.request();
     }
 
